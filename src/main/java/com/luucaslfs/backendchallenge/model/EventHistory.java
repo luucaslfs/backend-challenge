6d935820a48870e862dabfcffc06ed21218764aa
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-03-22 20:19:35 -0300

    refactor: cleaning jpa model entities

--- src/main/java/com/luucaslfs/backendchallenge/model/EventHistory.java
+++ src/main/java/com/luucaslfs/backendchallenge/model/EventHistory.java
@@ -2,17 +2,14 @@ package com.luucaslfs.backendchallenge.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.Hibernate;
 
 import java.sql.Timestamp;
-import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
 @Entity
 @Table(name = "event_history")
 public class EventHistory {
@@ -32,16 +29,4 @@ public class EventHistory {
     @Column(name = "created_at")
     private Timestamp createdAt;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        EventHistory that = (EventHistory) o;
-        return id != -1 && Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
