4ddfaef5b11e601ae91d5e7bab4fdf0103c758c9
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-04-03 16:38:50 -0300

    feat: remaking the rabbitMQ broker setup

--- src/main/java/com/luucaslfs/backendchallenge/broker/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.luucaslfs.backendchallenge.broker;
-
-
-public class BusinessException extends Exception{
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/luucaslfs/backendchallenge/broker/RabbitMQConfig.java
+++ src/main/java/com/luucaslfs/backendchallenge/broker/RabbitMQConfig.java
@@ -1,22 +1,36 @@
 package com.luucaslfs.backendchallenge.broker;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    private static final String QUEUE_NAME = "subscription";
+    private static final String QUEUE_NAME = "subscription.v1.subscription-update";
     private static final String EXCHANGE_NAME = "streaming-exchange";
     private static final String ROUTING_KEY = "subscription-status";
 
     @Bean
     public Queue testeQueue() {
         return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
     }
 
     @Bean
