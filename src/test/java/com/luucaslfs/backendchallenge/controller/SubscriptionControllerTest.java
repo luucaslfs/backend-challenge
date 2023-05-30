6b1bc96225ea2e40d1c8dcc2df58f98bd170f958
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-05-30 16:20:28 -0300

    tests: adding SubscriptionControllerTest

---
+++ src/test/java/com/luucaslfs/backendchallenge/controller/SubscriptionControllerTest.java
@@ -0,0 +1,56 @@
+package com.luucaslfs.backendchallenge.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.luucaslfs.backendchallenge.dto.SubscriptionDTO;
+import com.luucaslfs.backendchallenge.repository.SubscriptionRepository;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(MockitoJUnitRunner.class)
+class SubscriptionControllerTest {
+
+    private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectWriter objectWriter = objectMapper.writer();
+
+    @Mock
+    private SubscriptionRepository subscriptionRepository;
+
+    @InjectMocks
+    private SubscriptionController subscriptionController;
+
+    SubscriptionDTO RECORD_1 = SubscriptionDTO.builder()
+            .id(1L)
+            .build();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(subscriptionController).build();
+    }
+
+    @Test
+    void getAllSubscriptions() {
+    }
+
+    @Test
+    void registerSubscription() {
+    }
+
+    @Test
+    void updateSubscription() {
+    }
+
+    @Test
+    void updateSubscriptionStatus() {
+    }
+}
