92b7fc339b8a3bb30e9c6428e5143f868be8d023
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-05-31 00:07:10 -0300

    refactor: adding @Builder annotation to DTO classes

--- src/main/java/com/luucaslfs/backendchallenge/dto/SubscriptionDTO.java
+++ src/main/java/com/luucaslfs/backendchallenge/dto/SubscriptionDTO.java
@@ -1,9 +1,6 @@
 package com.luucaslfs.backendchallenge.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Timestamp;
 
@@ -11,6 +8,7 @@ import java.sql.Timestamp;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SubscriptionDTO {
 
     int id;
--- src/main/java/com/luucaslfs/backendchallenge/dto/UserDTO.java
+++ src/main/java/com/luucaslfs/backendchallenge/dto/UserDTO.java
@@ -1,15 +1,13 @@
 package com.luucaslfs.backendchallenge.dto;
 
 import com.luucaslfs.backendchallenge.model.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserDTO {
         int id;
         String fullName;
